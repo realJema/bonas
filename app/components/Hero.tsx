0fdad3199b726aabdf6901266eccef6d40f578c9
Md Christien <mdchristien12@gmail.com>
2024-08-05 17:37:35 +0100

    Build the hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useCallback } from "react";
+import { Swiper as SwiperType } from "swiper";
+import HeroSwiper from "./swiper/HeroSwipper";
+import CustomNavigation from "./swiper/Navigation";
+import HeroLink, { HeroLinkProps } from "./HeroLink";
+
+const heroLinksData: HeroLinkProps[] = [
+  {
+    href: "#",
+    title: "Get Matched with freelancers",
+    description: "create a brief and get custom offers.",
+    imageSrc:
+      "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/4058b3b3a6b0b3ac15f040c92bd2b05f-1704098481407/brief-and-match-icon.png",
+    imageAlt: "Freelancer matching icon",
+    message: "recommended for you",
+    badge: false,
+  },
+  {
+    href: "#",
+    title: "Reply to Abhi patal",
+    description: "Hi",
+    imageSrc:
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    imageAlt: "Tailor Fiverr icon",
+    message: "new message",
+    badge: true,
+  },
+  {
+    href: "#",
+    title: "Tailor Fiverr to your needs",
+    description: "Tell us about your needs",
+    imageSrc:
+      "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/093afbd0303654d5957e5ce787f7bd1f-1704031598839/Frame%201000001535.png",
+    imageAlt: "Tailor Fiverr icon",
+    message: "business recommendations",
+    badge: false,
+  },
+];
+
+const Hero = () => {
+  const [swiper, setSwiper] = useState<SwiperType | null>(null);
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
+  const handleSwiper = useCallback((swiperInstance: SwiperType) => {
+    setSwiper(swiperInstance);
+    setIsBeginning(swiperInstance.isBeginning);
+    setIsEnd(swiperInstance.isEnd);
+  }, []);
+
+  const handlePrev = useCallback(() => {
+    swiper?.slidePrev();
+    setIsBeginning(swiper?.isBeginning ?? true);
+    setIsEnd(swiper?.isEnd ?? false);
+  }, [swiper]);
+
+  const handleNext = useCallback(() => {
+    swiper?.slideNext();
+    setIsBeginning(swiper?.isBeginning ?? true);
+    setIsEnd(swiper?.isEnd ?? false);
+  }, [swiper]);
+
+  return (
+    <section className="hero mt-24 xl:mt-16 h-full py-10 px-8 rounded-2xl text-white">
+      <h1 className="text-white text-2xl md:text-[34px] font-semibold mb-8">
+        Welcome to Fiverr, Mesueh C 🎉
+      </h1>
+      <div className="hidden xl:flex items-center gap-4 w-full">
+        {heroLinksData.map((linkData, index) => (
+          <HeroLink
+            className="bg-green-50 bg-opacity-20"
+            key={index}
+            {...linkData}
+          />
+        ))}
+      </div>
+      <div className="hidden sm:block xl:hidden">
+        <div className="flex justify-end mb-2.5">
+          <CustomNavigation
+            bg="gray-100"
+            onNext={handleNext}
+            onPrev={handlePrev}
+            isBeginning={isBeginning}
+            isEnd={isEnd}
+          />
+        </div>
+        <HeroSwiper onSwiper={handleSwiper} slides={heroLinksData} />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ app/components/swiper/Navigation.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+interface NavigationProps {
+  containerStyles?: string;
+  styles?: string;
+  bg?: string;
+  svgSize?: string;
+  onPrev: () => void;
+  onNext: () => void;
+  isBeginning: boolean;
+  isEnd: boolean;
+}
+
+const Navigation = ({
+  containerStyles,
+  onPrev,
+  onNext,
+  styles,
+  bg = "bg-gray-50",
+  svgSize = "h-4 w-4",
+  isBeginning,
+  isEnd,
+}: NavigationProps) => {
+  return (
+    <div className={`flex items-center gap-3 ${containerStyles}`}>
+      <button
+        className={`prev-button z-10 bg-gray-50 bg-opacity-75 transition-colors hover:bg-gray-100 rounded-full w-8 h-8 flex items-center justify-center shadow-md ${
+          isBeginning ? "opacity-50 cursor-not-allowed" : ""
+        } ${styles}`}
+        onClick={onPrev}
+        disabled={isBeginning}
+      >
+        <svg
+          width="8"
+          height="15"
+          className={`${svgSize}`}
+          viewBox="0 0 8 15"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.2279 0.690653L7.84662 1.30934C7.99306 1.45578 7.99306 1.69322 7.84662 1.83968L2.19978 7.5L7.84662 13.1603C7.99306 13.3067 7.99306 13.5442 7.84662 13.6907L7.2279 14.3094C7.08147 14.4558 6.84403 14.4558 6.69756 14.3094L0.153374 7.76518C0.00693607 7.61875 0.00693607 7.38131 0.153374 7.23484L6.69756 0.690653C6.84403 0.544184 7.08147 0.544184 7.2279 0.690653Z"
+            fill="black"
+          />
+        </svg>
+      </button>
+
+      <button
+        onClick={onNext}
+        className={`next-button z-10 bg-gray-50 bg-opacity-75 transition-colors hover:bg-gray-100 rounded-full w-8 h-8 flex items-center justify-center shadow-md ${
+          isEnd ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+        disabled={isEnd}
+      >
+        <svg
+          width="8"
+          height="15"
+          className="h-4 w-4"
+          viewBox="0 0 8 15"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.772102 14.3093L0.153379 13.6906C0.00694026 13.5442 0.00694026 13.3067 0.153379 13.1603L5.80022 7.5L0.153379 1.83967C0.00694026 1.69323 0.00694026 1.45577 0.153379 1.30931L0.772102 0.690585C0.918534 0.544151 1.15598 0.544151 1.30242 0.690585L7.84661 7.23482C7.99307 7.38125 7.99307 7.61869 7.84661 7.76516L1.30242 14.3093C1.15598 14.4558 0.918534 14.4558 0.772102 14.3093Z"
+            fill="black"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Navigation;

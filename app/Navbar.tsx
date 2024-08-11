2dc3130e5d02ee08eab305390b3b12bccf14b953
Md Christien <mdchristien12@gmail.com>
2024-08-11 21:57:52 +0100

    refactoring: - change logo - refactor search input component

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import profileImg from "@/public/mdprofile.jpg";
 import SearchInput from "./components/SearchInput";
 import Header from "./components/Header";
+import Logo from "./components/Logo";
 
 const handleSearch = (searchText: string) => console.log(searchText);
 
@@ -14,26 +15,14 @@ const Navbar = () => {
   return (
     <header className="border-b h-16 bg-white px-6 xl:px-0">
       <nav className="flex justify-around sm:justify-between items-center sticky z-40 top-0 border-b h-16 bg-white gap-5 sm:gap-3 container mx-auto md:max-w-7xl">
-        <div className="flex items-center gap-3 md:gap-3 w-full">
+        <div className="flex items-center gap-4 md:gap-7 w-full">
           <div className="flex items-center gap-3">
             <div className="xl:hidden">
               <Sidebar />
             </div>
+            {/* logo */}
             <Link className="hidden sm:block" href="/">
-              <svg
-                width="89"
-                height="27"
-                viewBox="0 0 89 27"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="#404145">
-                  <path d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z"></path>
-                </g>
-                <g fill="#1dbf73">
-                  <path d="m85.3 27c2 0 3.7-1.7 3.7-3.7s-1.7-3.7-3.7-3.7-3.7 1.7-3.7 3.7 1.7 3.7 3.7 3.7z"></path>
-                </g>
-              </svg>
+              <Logo />
             </Link>
           </div>
 
@@ -45,21 +34,7 @@ const Navbar = () => {
 
         {/* logo */}
         <Link className="sm:hidden" href="/">
-          {" "}
-          <svg
-            width="89"
-            height="27"
-            viewBox="0 0 89 27"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g fill="#404145">
-              <path d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z"></path>
-            </g>
-            <g fill="#1dbf73">
-              <path d="m85.3 27c2 0 3.7-1.7 3.7-3.7s-1.7-3.7-3.7-3.7-3.7 1.7-3.7 3.7 1.7 3.7 3.7 3.7z"></path>
-            </g>
-          </svg>
+          <Logo />
         </Link>
 
         <div className="flex items-center gap-4">
---
+++ app/components/Logo.tsx
@@ -0,0 +1,26 @@
+
+const Logo = () => {
+  return (
+    <h1 className="text-2xl font-black text-black opacity-90 flex items-center">
+      <span className="relative">
+        Bonas
+        <span className="absolute bottom-2 -right-2">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.29998 8.00001C6.29998 8.00001 7.99998 6.3 7.99998 4.3C7.99998 2.3 6.29998 0.600006 4.29998 0.600006C2.29998 0.600006 0.599976 2.3 0.599976 4.3C0.599976 6.3 2.29998 8.00001 4.29998 8.00001Z"
+              fill="#1DBF73"
+            />
+          </svg>
+        </span>
+      </span>
+    </h1>
+  );
+};
+
+export default Logo;

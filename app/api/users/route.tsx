28673627368fe6cea7b9c560db39ad7542553520
Md Christien <mdchristien12@gmail.com>
2024-08-15 19:28:49 +0100

    Add the prisma adapter

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -5,7 +5,7 @@ import prisma from "@/prisma/client";
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany();
 
-  return NextResponse.json({id: 1 , name: 'me'});
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
--- app/components/auth/SignInModal.tsx
+++ app/components/auth/SignInModal.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const SignInModal = () => {
@@ -19,18 +20,19 @@ const SignInModal = () => {
                   Sign in
                 </h1>
                 <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
-                  Don't have an account yet?
-                  <a
-                    className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
+                  Don&apos;t have an account yet?
+                  <Link
+                    className="text-green-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-green-500"
                     href="../examples/html/signup.html"
                   >
                     Sign up here
-                  </a>
+                  </Link>
                 </p>
               </div>
 
               <div className="mt-5">
-                <button
+                <Link
+                  href="/api/auth/signin"
                   type="button"
                   className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
                 >
@@ -59,7 +61,7 @@ const SignInModal = () => {
                     />
                   </svg>
                   Sign in with Google
-                </button>
+                </Link>
 
                 <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
                   Or
@@ -76,7 +78,7 @@ const SignInModal = () => {
                           type="email"
                           id="email"
                           name="email"
-                          className="py-3 px-4 block w-full border-gray-900 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                          className="py-3 px-4 block w-full border border-gray-700 rounded-lg text-sm focus:border-green-500 focus:ring-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                           aria-describedby="email-error"
                         />
                         <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
@@ -106,20 +108,19 @@ const SignInModal = () => {
                         <label className="block text-sm mb-2 dark:text-white">
                           Password
                         </label>
-                        <a
-                          className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
+                        <Link
+                          className="inline-flex items-center gap-x-1 text-sm text-green-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-green-500"
                           href="../examples/html/recover-account.html"
                         >
                           Forgot password?
-                        </a>
+                        </Link>
                       </div>
                       <div className="relative">
                         <input
                           type="password"
                           id="password"
                           name="password"
-                          className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                          required
+                          className="py-3 px-4 block w-full border border-gray-700 rounded-lg text-sm focus:border-green-500 focus:ring-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                           aria-describedby="password-error"
                         />
                         <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
@@ -149,7 +150,7 @@ const SignInModal = () => {
                           id="remember-me"
                           name="remember-me"
                           type="checkbox"
-                          className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                          className="shrink-0 mt-0.5 border-gray-200 rounded text-green-600 focus:ring-green-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
                         />
                       </div>
                       <div className="ms-3">
@@ -161,7 +162,7 @@ const SignInModal = () => {
 
                     <button
                       type="submit"
-                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-green-600 text-white hover:bg-green-700 focus:outline-none focus:bg-green-700 disabled:opacity-50 disabled:pointer-events-none"
                     >
                       Sign in
                     </button>

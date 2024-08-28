9efa33230b1c8c3f12b20abf6ddcbc83de1eafae
Md Christien <mdchristien12@gmail.com>
2024-08-28 04:54:19 +0100

    Add content to job and real-eastate api's

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,14 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import schema from '../schema'
+import schema from "../schema";
 import prisma from "@/prisma/client";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
 
   if (!user) return NextResponse.json({ message: "User Not found" });
@@ -16,20 +15,18 @@ export async function GET(
   return NextResponse.json(user);
 }
 
-
 export async function PUT(
   request: NextRequest,
   { params: { id } }: { params: { id: number } }
 ) {
-   
   const body = await request.json();
   const validation = schema.safeParse(body);
 
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, {status: 400})
+    return NextResponse.json(validation.error.errors, { status: 400 });
 
   if (id > 10)
-    return NextResponse.json({error: 'User Not Found' }, {status: 404})
+    return NextResponse.json({ error: "User Not Found" }, { status: 404 });
 
-  return NextResponse.json({id: 1, name: body.name})
+  return NextResponse.json({ id: 1, name: body.name });
 }

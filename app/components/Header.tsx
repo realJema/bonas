d7853056fbe302a48b2293087ed240fdcfb847eb
Md Christien <mdchristien12@gmail.com>
2024-09-24 16:33:25 +0100

    display a loading skeleton while categories are being fethced

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -99,7 +99,7 @@ const Header = () => {
     router.push(url);
   };
 
-  if (isLoading) return <div>Loading categories...</div>;
+  if (isLoading) return <HeaderSkeleton />;
   if (error) return <div>Error loading categories: {error.message}</div>;
 
   console.log("fetched categories: ", categories);
@@ -194,3 +194,29 @@ const Header = () => {
 };
 
 export default Header;
+
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const HeaderSkeleton = () => {
+  return (
+    <header className="hidden md:block border-b xl:px-0 relative h-14">
+      <div className="hidden sm:block container mx-auto md:max-w-7xl relative h-full">
+        <div className="relative h-full flex items-center justify-center">
+          <div className="overflow-x-auto whitespace-nowrap scrollbar-hide px-6">
+            {[...Array(10)].map((_, index) => (
+              <div key={index} className="inline-flex mr-6 last:mr-0">
+                <Skeleton width={100} height={18} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="sm:hidden p-4">
+        <Skeleton height={40} />
+      </div>
+    </header>
+  );
+};
+

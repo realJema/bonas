d0267fde18a4cb40f05066a8642128114d1a4d84
Md Christien <mdchristien12@gmail.com>
2024-09-25 05:32:49 +0100

    Fix combo box errors

--- app/categories/[...category]/Search.tsx
+++ app/categories/[...category]/Search.tsx
@@ -23,8 +23,6 @@ const Search = () => {
       <input
         className="py-3 ps-10 pe-4 block w-full border border-gray-300 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
         type="text"
-        role="combobox"
-        aria-expanded="false"
         placeholder="Type a category name"
       />
     </div>

185adc7ca9d0bd7ba17864742b6b2871bf85c08d
Md Christien <mdchristien12@gmail.com>
2024-09-25 18:35:07 +0100

    Fix swiper issue with Listing section component

--- app/components/ListingSection.tsx
+++ app/components/ListingSection.tsx
@@ -1,34 +1,24 @@
 "use client";
 
-import React, { ComponentType, useRef, useState } from "react";
-import TopRatedBadge from "./badges/TopRatedBadge";
-import ProBadge from "./badges/Probadge";
-import LevelBadge from "./badges/Levelbadge";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper as SwiperType } from "swiper/types";
+import "swiper/css";
+import "swiper/css/free-mode";
 import ItemCard from "./ItemCard";
-import CustomSwiper from "./swiper/CustomSwiper";
 import Heading from "./Heading";
 import SubHeading from "./SubHeading";
-import { Swiper as SwiperInstance } from "swiper";
-import { Swiper as SwiperType } from "swiper/types";
 import Link from "next/link";
-import { Listing, Category, User, Image } from "@prisma/client";
 import { ExtendedListing } from "../entities/ExtendedListing";
-
-interface SlideItem {
-  type: "image" | "video";
-  url: string;
-}
 
 interface ListingSectionProps {
   heading: string;
   href: string;
   subheading: string;
   listings: ExtendedListing[];
-  generateSlides: (listing: ExtendedListing) => SlideItem[];
-  getBadgeComponent?: (
+  generateSlides: (
     listing: ExtendedListing
-  ) => ComponentType<any>;
-  offersVideo?: (listing: ExtendedListing) => boolean;
+  ) => { type: "image" | "video"; url: string }[];
 }
 
 const ListingSection = ({
@@ -37,10 +27,8 @@ const ListingSection = ({
   subheading,
   listings,
   generateSlides,
-  getBadgeComponent,
-  offersVideo,
 }: ListingSectionProps) => {
-  const swiperRef = useRef<SwiperInstance | null>(null);
+  const swiperRef = useRef<SwiperType | null>(null);
   const [isBeginning, setIsBeginning] = useState(true);
   const [isEnd, setIsEnd] = useState(false);
 
@@ -56,13 +44,13 @@ const ListingSection = ({
     }
   };
 
-  const handleSlideChange = (swiper: SwiperType) => {
+  const updateNavStatus = (swiper: SwiperType) => {
     setIsBeginning(swiper.isBeginning);
     setIsEnd(swiper.isEnd);
   };
 
   return (
-    <div className="flex flex-col space-y-6 px-4 py-5">
+    <div className="flex flex-col space-y-6 px-4 py-5 bg-[#fafafa]">
       <Heading label={heading} />
       <div className="flex items-center gap-2 justify-between">
         <SubHeading label={subheading} />
@@ -71,68 +59,72 @@ const ListingSection = ({
         </Link>
       </div>
 
-    <div className="relative w-full">
-        <CustomSwiper
-          data={listings}
-          renderItem={(listing: ExtendedListing) => (
-            <div className="w-[230px]">
-              <ItemCard
-                listing={listing}
-                slides={generateSlides(listing)}
-                // Badge={getBadgeComponent(listing)}
-                // offersVideo={offersVideo(listing)}
-              />
-            </div>
-          )}
-          hasOverlayRight={false}
-          hasOverlayLeft={false}
+      <div className="relative z-10">
+        <Swiper
+          spaceBetween={3}
+          slidesPerView="auto"
           onSwiper={(swiper) => {
             swiperRef.current = swiper;
-            handleSlideChange(swiper);
+            updateNavStatus(swiper);
           }}
-          spaceBetween={3}
-          onSlideChange={handleSlideChange}
-        />
-
-        <button
-          className={`prev-button absolute -left-10 top-1/2 transform -translate-y-1/2 bg-white transition-colors hover:bg-gray-50 rounded-full z-20 w-12 h-12 flex items-center justify-center shadow-md ${
-            isBeginning ? "hidden" : ""
-          }`}
-          onClick={handlePrev}
+          onSlideChange={updateNavStatus}
+          className="listing-swiper"
         >
-          <svg
-            width="8"
-            height="15"
-            className="h-4 w-4 text-gray-600"
-            viewBox="0 0 8 15"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.2279 0.690653L7.84662 1.30934C7.99306 1.45578 7.99306 1.69322 7.84662 1.83968L2.19978 7.5L7.84662 13.1603C7.99306 13.3067 7.99306 13.5442 7.84662 13.6907L7.2279 14.3094C7.08147 14.4558 6.84403 14.4558 6.69756 14.3094L0.153374 7.76518C0.00693607 7.61875 0.00693607 7.38131 0.153374 7.23484L6.69756 0.690653C6.84403 0.544184 7.08147 0.544184 7.2279 0.690653Z"
-              fill="currentColor"
-            />
-          </svg>
-        </button>
+          {listings.map((listing, index) => (
+            <SwiperSlide key={index} className="!w-auto">
+              <div className="w-[240px]">
+                <div className="pr-4">
+                  <ItemCard
+                    listing={listing}
+                    slides={generateSlides(listing)}
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-        <button
-          onClick={handleNext}
-          className={`next-button absolute -right-5 top-1/2 transform -translate-y-1/2 bg-white transition-colors hover:bg-gray-50 rounded-full z-20 w-12 h-12 flex items-center justify-center shadow-md ${
-            isEnd ? "hidden" : ""
-          }`}
-        >
-          <svg
-            width="8"
-            height="15"
-            className="h-4 w-4 text-gray-600"
-            viewBox="0 0 8 15"
-            xmlns="http://www.w3.org/2000/svg"
+        {!isEnd && (
+          <div className="absolute top-0 right-0 w-16 h-full bg-gradient-to-l from-white to-transparent z-20" />
+        )}
+
+        {!isBeginning && (
+          <button
+            className="absolute -left-10 top-1/2 transform -translate-y-1/2 bg-white hover:bg-gray-50 rounded-full z-30 w-12 h-12 flex items-center justify-center shadow-md"
+            onClick={handlePrev}
           >
-            <path
-              d="M0.772102 14.3093L0.153379 13.6906C0.00694026 13.5442 0.00694026 13.3067 0.153379 13.1603L5.80022 7.5L0.153379 1.83967C0.00694026 1.69323 0.00694026 1.45577 0.153379 1.30931L0.772102 0.690585C0.918534 0.544151 1.15598 0.544151 1.30242 0.690585L7.84661 7.23482C7.99307 7.38125 7.99307 7.61869 7.84661 7.76516L1.30242 14.3093C1.15598 14.4558 0.918534 14.4558 0.772102 14.3093Z"
-              fill="currentColor"
-            />
-          </svg>
-        </button>
+            <svg
+              width="8"
+              height="15"
+              viewBox="0 0 8 15"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.2279 0.690653L7.84662 1.30934C7.99306 1.45578 7.99306 1.69322 7.84662 1.83968L2.19978 7.5L7.84662 13.1603C7.99306 13.3067 7.99306 13.5442 7.84662 13.6907L7.2279 14.3094C7.08147 14.4558 6.84403 14.4558 6.69756 14.3094L0.153374 7.76518C0.00693607 7.61875 0.00693607 7.38131 0.153374 7.23484L6.69756 0.690653C6.84403 0.544184 7.08147 0.544184 7.2279 0.690653Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+        )}
+
+        {!isEnd && (
+          <button
+            className="absolute -right-5 top-1/2 transform -translate-y-1/2 bg-white hover:bg-gray-50 rounded-full z-30 w-12 h-12 flex items-center justify-center shadow-md"
+            onClick={handleNext}
+          >
+            <svg
+              width="8"
+              height="15"
+              viewBox="0 0 8 15"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.772102 14.3093L0.153379 13.6906C0.00694026 13.5442 0.00694026 13.3067 0.153379 13.1603L5.80022 7.5L0.153379 1.83967C0.00694026 1.69323 0.00694026 1.45577 0.153379 1.30931L0.772102 0.690585C0.918534 0.544151 1.15598 0.544151 1.30242 0.690585L7.84661 7.23482C7.99307 7.38125 7.99307 7.61869 7.84661 7.76516L1.30242 14.3093C1.15598 14.4558 0.918534 14.4558 0.772102 14.3093Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+        )}
       </div>
     </div>
   );

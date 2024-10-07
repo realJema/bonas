cd22298eddff4ef429803a050da2d72798a7655f
Md Christien <mdchristien12@gmail.com>
2024-10-07 13:53:05 +0100

    Build the business info page

---
+++ app/(protected)/profile/[username]/settings/edit/business-information/VerifyButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const VerifyButton = () => {
+  return (
+    <Button size="xl" className="text-white bg-black hover:opacity-75 px-6 mt-4 rounded-md">
+      Veify
+    </Button>
+  );
+};
+
+export default VerifyButton;
--- app/(protected)/profile/[username]/settings/edit/business-information/page.tsx
+++ app/(protected)/profile/[username]/settings/edit/business-information/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import VerifyButton from "./VerifyButton";
 
 const BusinessInformationEditPage = () => {
   return (
-    <div>BusinessInformationEditPage</div>
-  )
-}
+    <div className="bg-white flex-col space-y-6 p-8 mt-6 rounded-md">
+      <div className="flex-col space-y-2">
+        <h1 className="text-black text-xl font-semibold">
+          Personal & business information
+        </h1>
+        <p>
+          Verify your personal and business information for compliance purposes.
+          <Link href="#" className="underline inline-block">
+            Learn more
+          </Link>
+        </p>
+      </div>
+      <VerifyButton />
+    </div>
+  );
+};
 
-export default BusinessInformationEditPage
+export default BusinessInformationEditPage;

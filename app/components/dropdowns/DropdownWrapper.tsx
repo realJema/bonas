96e16c5e0067389268eafd2bc3ad5cab03b6f3dd
Md Christien <mdchristien12@gmail.com>
2024-11-19 15:03:09 +0100

    refactor: Increase dropdown length

--- app/components/dropdowns/DropdownWrapper.tsx
+++ app/components/dropdowns/DropdownWrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const DropdownWrapper = ({ children }: PropsWithChildren) => {
   return (
-    <div className="absolute left-0 top-full w-full max-w-5xl bg-white shadow-sm overflow-hidden z-30">
+    <div className="absolute left-0 top-full w-full max-w-7xl bg-white shadow-sm overflow-hidden z-30">
       <div className="p-[2px] bg-gray-300">
         <div className="bg-white p-6">{children}</div>
       </div>
